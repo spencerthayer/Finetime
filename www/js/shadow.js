ffa0f0f117be090f53730368049997307810df85
Spencer Thayer <me@spencerthayer.com>
2015-06-30 15:24:08 -0500

    Text shadow improvements.

--- www/js/shadow.js
+++ www/js/shadow.js
@@ -4,17 +4,20 @@
 /**////////////////////
 function shadowMove() {
     var shadowObject = $('.shadow')
-    var textCenterTop = (shadowObject.offset().top + (shadowObject.height() /2))/2.5;
-    var textCenterLeft = (shadowObject.offset().left + (shadowObject.width() /2))/2.5;
-//  var mx = e.clientX;
-//  var my = e.clientY;
-    var deltaX = (/*textCenterLeft+*/sunx)*-1;
-    var deltaY = (/*textCenterTop+*/suny)*-1;
+    var textCenterTop = (shadowObject.offset().top + (shadowObject.height() /2));
+    var textCenterLeft = (shadowObject.offset().left + (shadowObject.width() /2));
+    var deltaX = (sunx)*-1;
+    var deltaY = (suny)*-1;
+    var deltaVar = .025;
     var shadowBlur = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
+    var shadowXCalc = deltaX*deltaVar;
+    var shadowYCalc = deltaY*deltaVar;
+    var shadowBlurCalc = shadowBlur*deltaVar*2;
+    var shadowColor = "rgba(25,0,75,1)";
     shadowObject.css({
-        "text-shadow" : deltaX / 20 + "px " + deltaY / 20 + "px " + shadowBlur / 20 +"px " + "rgba(25,0,100,.5)",
-        "-webkit-text-shadow" : deltaX / 20 + "px " + deltaY / 20 + "px " + shadowBlur / 20 +"px " + "rgba(25,0,100,.5)",
-        "-moz-text-shadow" : deltaX / 20 + "px " + deltaY / 20 + "px " + shadowBlur / 20 +"px " + "rgba(25,0,100,.5)",
-        "-o-text-shadow" : deltaX / 20 + "px " + deltaY / 20 + "px " + shadowBlur / 20 +"px " + "rgba(25,0,100,.5)"
+        "text-shadow": shadowXCalc+"vmin "+shadowYCalc+"vmin " +shadowBlurCalc+"vmin "+shadowColor,
+        "-webkit-text-shadow": shadowXCalc+"vmin "+shadowYCalc+"vmin " +shadowBlurCalc+"vmin "+shadowColor,
+        "-moz-text-shadow": shadowXCalc+"vmin "+shadowYCalc+"vmin " +shadowBlurCalc+"vmin "+shadowColor,
+        "-o-text-shadow": shadowXCalc+"vmin "+shadowYCalc+"vmin " +shadowBlurCalc+"vmin "+shadowColor
     });
 }
